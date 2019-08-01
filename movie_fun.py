88f6f3cdc6be41a29d4d2b767abd2f360dc3f929
Gal Gilor <gilorgal2010@gmail.com>
2019-07-31 20:36:35 -0400

    inserting helper functions to function module

--- movie_fun.py
+++ movie_fun.py
@@ -164,3 +164,284 @@ def get_genres():
 #################################################################################
 
 # omdbi api helper functions
+
+
+def convert_to_int(string):
+    '''This function tries to convert a string into an integer, if it fails
+       it will return "null"'''
+    try:
+        clean_string = re.sub('\W+','', string)
+        clean_string = int(clean_string)
+    except:
+        clean_string = "NULL"
+    return clean_string
+
+
+def rt_rating(movie):
+    """This function is able to access the inner dictionary from each movie
+       json file to retrieve it rotten tomatoes score"""
+    for rating in movie['Ratings']:
+        if rating['Source'] == "Rotten Tomatoes":
+            rt_v = rating['Value']
+            return convert_to_int(rt_v)
+
+
+def convert_list_from_tuples(tuple_list):
+    """The movie titles are retrieved as a list of tupes, 
+    return instead a list with just the title from the first half of each tuple"""
+    title_list = []
+    for item in tuple_list:
+        title_list.append(item[0])
+    return title_list
+    
+
+def get_all_movie_info():
+    '''This function grabs all the data from inside
+       the "movies" table in movie db'''
+    select_q = """
+                SELECT *
+                FROM movies
+                """
+    cnx, c = connect_to_db()
+    c.execute(q)
+    results = c.fetchall()
+    cnx.close()
+    c.close()
+    return results
+
+def get_movie_names():
+    '''This function grabs the title from inside
+       the "movies" table in movie db'''
+    name_q = """
+            SELECT title
+            FROM movies"""
+    cnx, c = connect_to_db()
+    c.execute(name_q)
+    results = c.fetchall()
+    cnx.close()
+    c.close()
+    title_list = convert_list_from_tuples(results) 
+    return title_list
+
+
+def check_data_fields(data, list_of_fields):
+    """Takes in a dictionary and a list of all keys that must be in that dictionary
+        returns false if any of those keys are not contained"""
+    exists = True
+    for field in list_of_fields:
+        if field not in data:
+            exists = False        
+    return exists
+
+def omdb_api(movies):
+    """This function retrieves the movie deatils for list of given movies using omdb api"""
+    movies_details = []
+    for title in movies:
+        title.replace(" ", "+")
+        url = f'http://www.omdbapi.com/?apikey={config.omdb_api}&t={title}'
+        response = requests.get(url)
+        movie = response.json()
+        #check to make sure each key is always in the dict
+        necessary_fields = ['Director', 'BoxOffice', 'Ratings', 'Title']
+        if check_data_fields(movie, necessary_fields):
+            #4 necessary fields, title is important for the insert statement query later
+            movie_dict = {'director': movie['Director'],
+                          'boxoffice': convert_to_int(movie['BoxOffice']),
+                          'rt_rating': rt_rating(movie),
+                          'title': movie['Title']}
+            movies_details.append(movie_dict)
+        time.sleep(.2)
+        
+    
+    return movies_details 
+
+
+def add_omdb_values_to_movies():
+    """Add box office, director and rotten tomatoes rating 
+    from the omdb to our pre-existing movies DB"""
+    
+    #retrieve all movie names and pass to omdb_api to get a 
+    #list of dictionaries with values of directora nd box_office and rating
+    all_titles = get_movie_names()
+    omdb_values = omdb_api(all_titles)
+    return omdb_values
+
+
+
+#to account for when rt_rating or box_office doesn't exist, we have different sql statements for each case
+def determing_insert_query(values):
+    director = values[0]
+    box_office = values[1]
+    rt_rating = values[2]
+    title = values[3]
+    #both box office and rating are absent
+    if box_office == "NULL" and rt_rating == None:
+        return f"""UPDATE movies
+                    SET 
+                        director = "{director}"
+                    WHERE title = "{title}";"""
+    #box office is absent
+    elif box_office == 'NULL':
+        return f"""UPDATE movies
+                    SET 
+                        director = "{director}", 
+                        rt_ratings = '{rt_rating}'
+                    WHERE title = "{title}";"""
+    #rating is absent
+    elif rt_rating == None:
+        return f"""UPDATE movies
+                    SET 
+                        director = "{director}", 
+                        box_office = '{box_office}'
+                    WHERE title = "{title}";"""
+    #all accounted for
+    else:
+        return f"""UPDATE movies
+                    SET 
+                        director = "{director}", 
+                        box_office = '{box_office}', 
+                        rt_ratings = '{rt_rating}'
+                    WHERE title = "{title}";"""
+
+
+def insert_omdb_values(values):
+    '''This function inserts the values "director", 
+       "box_office" (revenue), and rt_ratings into the db'''
+    insert_q = determing_insert_query(values)
+    cnx, c = connect_to_db()
+    c.execute(insert_q)
+    cnx.commit()
+    c.close
+    cnx.close
+
+
+#to test to see if title = {title} actually matches anything
+def check_title_match(values):
+    title = values[3]
+    insert_q = f"""SELECT title, movie_id
+            FROM movies
+            WHERE title = "{title}";"""
+    cnx, c = connect_to_db()
+    c.execute(insert_q)
+    result = c.fetchall()
+    c.close
+    cnx.close
+    return result
+
+
+#################################################################################
+#TMDb_api helper functions   
+
+#parse through the data and retrieve only what I need
+def arrange_data(data):
+    """This function receives the raw data that we pulled. that is saved in a list of pages.
+       each page contains a list of movies for every movie """
+    new_list = []
+    for page in data:
+        for dictionary in page:
+            #save the values we want for each title
+            new_dictionary = {}
+            for k, v in dictionary.items():
+                if k == 'id':
+                    new_dictionary[k] = v
+                if k == 'title':
+                    new_dictionary[k] = v
+                if k == 'popularity':
+                    new_dictionary[k] = v
+                if k == 'release_date':
+                    new_dictionary[k] = v
+                if k == 'vote_count':
+                    new_dictionary[k] = v
+                if k == 'vote_average':
+                    new_dictionary[k] = v
+                if k == 'genre_ids':
+                    new_dictionary[k] = v
+            new_list.append(new_dictionary)
+    return new_list
+
+###############################
+#these two functions make sure our data is in tuple form
+def create_tuple(reducted_data):
+    data = (reducted_data['id'], reducted_data['title'],
+             reducted_data['popularity'], reducted_data['release_date'],
+             reducted_data['vote_count'], reducted_data['vote_average'])
+
+    return data
+
+def multiple_tuples(reducted_data):
+    list_tuples = []
+    for movie in reducted_data:
+        list_tuples.append(create_tuple(movie))
+    return list_tuples
+################################
+
+#'movies' table insertion function
+def db_insertion(query, data):
+    '''This function inserts one instance of data into our db'''
+    try:
+        for datum in data:
+            # Make sure data is committed to the database
+            cursor.execute(query, datum)
+            cnx.commit()
+    except:
+        print(f"{datum} already exists!")
+    pass
+
+def top_rated(pages):
+    """This function retrieves the top rated movies from the movie db using their api.
+       The user is required to give it a list of the pages they want to obtain from the db"""
+    
+    url = f'https://api.themoviedb.org/3/movie/top_rated?api_key={config.api_key}'
+    top_rated = []
+    for page in pages:
+        #updating the 'page' paramater
+        params = {'language': '', 'page': page}
+        response = requests.get(url, params=params)
+        json_response = response.json()
+        #making sure we have data before going on
+        if 'results' in json_response:
+            top_rated.append(json_response['results'])
+        #considering movieDB limit for requests
+        time.sleep(4)
+    #returns a list of dictionary. each page on a different dictionary.
+    reducted_data = arrange_data(top_rated)
+    #tupple_data = multiple_tuples(reducted_data)
+    return  reducted_data
+
+
+def populate_movies_table(data):
+    cnx, cursor = connect_to_db()
+    #the query we use to insert
+    insert_movies_q = ("""INSERT INTO movies 
+                     (movie_id, title, popularity, 
+                     release_date, vote_count, vote_avg)
+                     VALUES (%s, %s, %s, %s, %s, %s)""")
+
+    #get the full list of all values to be inserted
+    tupled_data = multiple_tuples(data)
+    # insert the business data ito db
+    db_insertion(insert_movies_q, tupled_data)
+
+    # make sure the connection is closed.
+    cursor.close()
+    cnx.close()
+
+
+#helper functiosn in populating the genre instances table
+def create_genre_tuple(reducted_data):
+    data = (reducted_data['id'], reducted_data['genre_ids'])
+    return data
+
+def genres_tuples(reducted_data):
+    list_tuples = []
+    for movie in reducted_data:
+        list_tuples.append(create_genre_tuple(movie))
+    return list_tuples
+
+def genre_instances_insertion(query, data):
+    for datum in data:
+        movie_id = datum[0]
+        for genre_id in datum[1]:
+            cursor.execute(query, (movie_id, genre_id))
+            cnx.commit()
+        
